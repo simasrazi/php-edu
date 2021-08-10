b2e2146004ec8c686e89afa3e80bda4e0e460817
simasrazi <66324929+simasrazi@users.noreply.github.com>
2021-08-10 15:04:43 +0300

    Update ordered-arrays.php

--- ordered-arrays.php
+++ ordered-arrays.php
@@ -46,3 +46,13 @@ array_shift($record_holders);
 array_unshift($record_holders, "Usain Bolt");
 
 // Nested Arrays
+$treasure_hunt = 
+["garbage", "cat", 99, 
+  ["soda can", 8, ":)", "sludge", 
+    ["stuff", "lint", 
+      ["GOLD!"],
+    "cave", "bat", "scorpion"],
+  "rock"],
+"glitter", "moonlight", 2.11];
+  
+echo $treasure_hunt[3][4][2][0];
